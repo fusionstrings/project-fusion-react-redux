57d7c1be2787fe1854e8cb3c11e89c5a6daee3c9
unknown <dshe8z@RD0100241.global.scd.scania.com>
2016-01-07 10:50:19 +0100

    make esdoc es7 compatible

--- gulp/tasks/esdoc.js
+++ gulp/tasks/esdoc.js
@@ -7,11 +7,21 @@ const $ = gulpLoadPlugins();
 //Generate JavaScript docs
 gulp.task('esdoc', () => {
   gulp.src(paths.root)
-    .pipe($.esdoc({ destination: paths.tmpEsdoc }));
+    .pipe($.esdoc({
+      destination: paths.tmpEsdoc,
+      'plugins': [
+        {"name": "esdoc-es7-plugin"}
+      ]
+    }));
 });
 
 //Generate JavaScript docs for deployment
 gulp.task('esdoc:dist', () => {
   gulp.src(paths.root)
-    .pipe($.esdoc({ destination: paths.outputEsdoc }));
+    .pipe($.esdoc({
+      destination: paths.outputEsdoc,
+      'plugins': [
+        {"name": "esdoc-es7-plugin"}
+      ]
+    }));
 });
